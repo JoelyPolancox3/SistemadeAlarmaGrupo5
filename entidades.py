34e872d13e073708b8a286dab0289d5957fbf5db
JoelyPolancox3 <chadcharela@gmail.com>
2026-09-07 21:14:40 -0600

    mmm

--- entidades.py
+++ entidades.py
@@ -18,6 +18,30 @@ class Usuario:
     def __repr__(self):
         return f"Usuario(id={self.id}, nombre='{self.nombre}', email='{self.email}')"
 
+#Entidad Incidencia
+@dataclass
+class Incidencia:  
+    id: Optional[int] = None
+    usuario_id: int = 0
+    Titulo: str = ""
+    Descripcion: str= ""
+    Estado: str = "Pendiente"
+    Fecha_Creacion: datetime = datetime.now()
+    prioridad: str = "media"
 
+def __repr__(self):
+        return f"Incidencia(id={self.id}, titulo='{self.titulo}', estado='{self.estado}')"
 
+#Entidad cliente
+@dataclass
+class Cliente:
+    id: Optional[int] = None
+    nombre: str = ""
+    email: str = ""
+    telefono: str = ""
+    empresa: str = ""
+    sector: str = "otros"
+
+    def __repr__(self):
+        return f"Cliente(id={self.id}, nombre='{self.nombre}', email='{self.email}')"
 
